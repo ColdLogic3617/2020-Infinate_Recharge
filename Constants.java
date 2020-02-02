383bfc5c9ce30228d61fe667cc7d6fc261bdac30
Team Cold Logic <coldlogic3617@gmail.com>
2020-02-02 11:25:45 -0800

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import java.util.ArrayList;
+import edu.wpi.first.wpilibj.*;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    //declaring Joysticks
+   /* public static Joystick leftStick = new Joystick(0);
+    public static Joystick rightStick = new Joystick(1);
+    public static final Spark one = new Spark(0);
+    public static final Spark two = new Spark(1);
+    public static final Spark three = new Spark(2);
+    public static final Spark four = new Spark(3);
+    */
+
+    public Constants(int numControllers)
+    {
+
+    }
+
+   /* public static PWMSpeedController getController(int num)
+    {
+        
+    }*/
+}
